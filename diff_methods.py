b4b442b6c114a45c7a1cfe275c0a51fd3d0be150
MITHUN G P <mithungpgowda549@gmail.com>
2025-01-30 20:29:04 +0530

    Add files via upload

---
+++ diff_methods.py
@@ -0,0 +1,34 @@
+# class students:
+#     college = "Acharya"
+#     def __init__(self,m1,m2,m3):
+#         self.m1=m1
+#         self.m2=m2
+#         self.m3=m3
+
+#     def avg(self):
+#          return (self.m2+self.m1+self.m3)/3
+    
+#     @classmethod
+#     def getcollege(cls):
+#         print(cls.college)
+
+#     @staticmethod
+#     def info():
+#         print("in static method")
+#         print(students.college)
+
+
+# o1 = students(20,49,29)
+# o2 = students(87,94,84)
+# print(o1.avg())
+# students.getcollege()
+# students.info()
+
+# a=2
+# b=3
+# print(a.__add__(b))
+
+
+
+
+        
